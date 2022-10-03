728eedb7ad8fe239e4816f7eb44614892c9b233b
The Warriorr <114317474+anirbansarkar545@users.noreply.github.com>
2022-10-04 02:39:42 +0530

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Cybersecurity Analyst", "Quality Assurance Analyst", "Software Tester", "Data Scientist", "Freelancer", "YouTuber"],
+        strings: ["Cybersecurity Analyst", "Quality Assurance Analyst", "Software Tester", "Data Scientist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Cybersecurity Analyst", "Quality Assurance Analyst", "Software Tester", "Data Scientist", "Freelancer", "YouTuber"],
+        strings: ["Cybersecurity Analyst", "Quality Assurance Analyst", "Software Tester", "Data Scientist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
